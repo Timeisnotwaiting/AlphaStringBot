d52f6137410eeb7d3114137e15652bb74575ea06
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-06 22:55:10 +0530

    Update string.py

--- string.py
+++ string.py
@@ -6,4 +6,15 @@ API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-str = Client(":Cli:", API_ID, API_HASH, BOT_TOKEN)
+ses = Client(":Cli:", API_ID, API_HASH, BOT_TOKEN)
+
+@ses.on_message(filters.command("start") & filters.private)
+async def _start(_, m):
+    start(_, m)
+
+ses.start()
+get = ses.get_me()
+un = get.username
+idle()
+
+print(f"@{un} started successfully.....")
