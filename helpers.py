867fb3b75bc50ee465b15f01960a69e864297576
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-08 15:07:18 +0530

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,11 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+_TEXT_1 = """Welcome to Alpha Pyrogram String Session Generator.
+
+
+You can procees with bot's api values if you want , else you can proceed with your api values
+
+Bot has over 100+ API ID and HASH Saved , You can use them. 
+
+
+Press Button to start generating session!"""
