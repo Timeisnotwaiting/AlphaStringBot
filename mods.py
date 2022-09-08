9837a6c40f462701187e0ab1e027eeca9a987e33
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-08 15:27:41 +0530

    Update mods.py

--- mods.py
+++ mods.py
@@ -6,5 +6,5 @@ async def _start(_, m):
     _start_1 = await m.reply(_TEXT_1, reply_markup=_MARKUP_1)
    
 async def _without_api(_, m):
-    ph_no_msg = await bot.ask
+    ph_no_msg = await _.ask(_PHONE_NUMBER, filters=filters.text)
     
