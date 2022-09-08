a439dd6b07752990a1b10f93baf353ba7c08c8a2
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-08 15:15:33 +0530

    Update mods.py

--- mods.py
+++ mods.py
@@ -1,5 +1,8 @@
-from .string import *
+from string import *
 from pyrogram.errors import *
+from helpers import *
 
-async def start(_, m):
+async def _start(_, m):
+    await m.reply(_TEXT_1, reply_markup=_MARKUP_1)
+    
     
