fd01e724a8e16cf346c145ed3abdfc83c0e45965
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-08 15:34:33 +0530

    Update mods.py

--- mods.py
+++ mods.py
@@ -7,4 +7,20 @@ async def _start(_, m):
    
 async def _without_api(_, m):
     ph_no_msg = await _.ask(_PHONE_NUMBER, filters=filters.text)
+    if await cancelled(ph_no_msg):
+        return
+    number = ph_no_msg.text
+    
+  async def cancelled(m):
+    if "/cancel" in m.text:
+        await m.reply("Cancelled the Process!", quote=True, reply_markup=_MARKUP_1)
+        return True
+    elif "/restart" in m.text:
+        await m.reply("Restarted the Bot!", quote=True, reply_markup=_MARKUP_1)
+        return True
+    elif msg.text.startswith("/"): 
+        await m.reply("Cancelled the generation process!", quote=True)
+        return True
+    else:
+        return False
     
