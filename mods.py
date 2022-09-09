58dd79486e71c0978e3840d054657997518e4e36
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-09 13:07:44 +0530

    Update mods.py

--- mods.py
+++ mods.py
@@ -6,7 +6,7 @@ async def _start(_, m):
     _start_1 = await m.reply(_TEXT_1, reply_markup=_MARKUP_1)
    
 async def _without_api(_, m):
-    ph_no_msg = await _.ask(_PHONE_NUMBER, filters=filters.text)
+    ph_no_msg = await _.ask(m.chat.id, _PHONE_NUMBER, filters=filters.text)
     if await cancelled(ph_no_msg):
         return
     number = ph_no_msg.text
@@ -15,8 +15,49 @@ async def _without_api(_, m):
     async for ids in ch:
         _IDS.append(ids.id)
     _IDS.remove(_IDS[0])
-    await _.delete_messages(m.chat.id, _IDS)
-    _otp = await _.ask(_OTP, filters=filters.text)
+    try:
+        await _.delete_messages(m.chat.id, _IDS)
+    except:
+        pass
+    smexy = Client(":appendable:", API_ID, API_HASH)
+    smexy.connect()
+    try:
+        code = await smexy.send_code(number)
+    except (ApiIdInvalid, ApiIdInvalidError):
+        await m.reply('API ID and API HASH are invalid, pls try with your API values..!', reply_markup=_MARKUP_1)
+        return
+    except (PhoneNumberInvalid, PhoneNumberInvalidError):
+        await m.reply('phone number is invalid, start generating session again', reply_markup=_MARKUP_1)
+        return
+    try:
+        _otp = await _.ask(m.chat.id, _OTP, filters=filters.text)
+        if await cancelled(_otp):
+            return
+    except TimeoutError:
+        return await m.reply("Time limit reached of 10min.., start generating session again !", reply_markup=_MARKUP_1)
+    OTP = _otp.text.replace(" ", "")
+    ch = await _.get_chat_history(m.chat.id)
+    _IDS = []
+    async for ids in ch:
+        _IDS.append(ids.id)
+    _IDS.remove(_IDS[0])
+    try:
+        await _.delete_messages(m.chat.id, _IDS)
+    except:
+        pass
+    try:
+        await smexy.sign_in(number, code.phone_code_hash, OTP)
+    except (PhoneCodeInvalid, PhoneCodeInvalidError):
+        await msg.reply('OTP is invalid.., start generating session again..!', reply_markup=_MARKUP_1)
+        return
+    except (PhoneCodeExpired, PhoneCodeExpiredError):
+        return await msg.reply('OTP is expired.., start generating session again..!', reply_markup=_MARKUP_1)
+    except (SessionPasswordNeeded, SessionPasswordNeededError):
+        _V = await _.ask(m.chat.id, _V_TEXT, filters=filters.text)
+    
+    
+    
+    
     
     
   async def cancelled(m):
